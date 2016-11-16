35b3588379b624304e2dc035064587bbfc407a79
shiyujiucsb <shiyu@cs.ucsb.edu>
2016-11-16 03:52:34 -0800

    1116

--- heatEquationImplicitApproximate.py
+++ heatEquationImplicitApproximate.py
@@ -1,4 +1,4 @@
-# Explicit method to solve the head equation.
+# Implicit method to solve the heat equation.
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as pl
 import numpy as np
@@ -6,7 +6,7 @@ import matplotlib.patches as mpatches
 
 t0 = 0.0
 t_final = 1.0
-n_grid = 20
+n_grid = 100
 dt = t_final / n_grid
 dx = 1.0 / n_grid
 T = np.arange(t0, t_final+dt, dt)
@@ -31,28 +31,38 @@ for t in T:
         if x>0.0:
             ax.plot([t, t],[x-dx, x],[exact(t, x-dx), exact(t,x)], 'b-', linewidth=0.5)
 
-# Explicit method
+# Implicit method
 r = dx/(dt*dt)/(np.pi*np.pi)
 U = [[0.0 for _ in X] for _ in T]
+A = [[0.0 for _ in X] for _ in X]
+b = [0.0 for _ in X]
+for i in range(len(X)):
+    if i in [0, len(X)-1]:
+        A[i][i] = 1.0
+    else:
+        A[i][i] = 1+2*r
+        A[i][i-1] = -r
+        A[i][i+1] = -r
 for i in range(len(T)):
-    for j in range(len(X)):
-        if i==0:
+    if i==0:
+        for j in range(len(X)):
             U[i][j] = exact(T[i], X[j])
-        elif j==0 or j==len(X)-1:
-            U[i][j] = 0
-        else:
-            U[i][j] = (1-2*r)*U[i-1][j] + r*U[i-1][j-1] + r*U[i-1][j+1]
+            b[j] = U[i][j]
+    else:
+        b = np.linalg.lstsq(A, b)[0]
+        b[0], b[-1] = 0.0, 0.0
+        for j in range(len(X)):
+            U[i][j] = b[j]
 
 for i in range(len(T)):
     for j in range(len(X)):
-        if 0 < U[i][j]:  # select the approximate values within the bound to display
-            if i>0:
-                ax.plot([T[i-1], T[i]],[X[j], X[j]],[U[i-1][j], U[i][j]], 'r-', linewidth=0.5)
-            if j>0:
-                ax.plot([T[i], T[i]],[X[j-1], X[j]],[U[i][j-1], U[i][j]], 'r-', linewidth=0.5)
+        if i>0:
+            ax.plot([T[i-1], T[i]],[X[j], X[j]],[U[i-1][j], U[i][j]], 'r-', linewidth=0.5)
+        if j>0:
+            ax.plot([T[i], T[i]],[X[j-1], X[j]],[U[i][j-1], U[i][j]], 'r-', linewidth=0.5)
 
 bluePatch = mpatches.Patch(color='blue', label='Exact')
-redPatch = mpatches.Patch(color='red', label='Explicit')
+redPatch = mpatches.Patch(color='red', label='Implicit')
 pl.legend(handles = [bluePatch, redPatch], loc=2)
 
 pl.show()
